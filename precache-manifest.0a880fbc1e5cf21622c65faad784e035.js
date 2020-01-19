1f188246bcf824204e94a819e225ed044f7cfb5f
Marcin Stenka <mastenka@wp.pl>
2020-01-19 22:19:12 +0100

    Updates

---
+++ precache-manifest.0a880fbc1e5cf21622c65faad784e035.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065547b6746c9c0347ddb55318ca4dc4",
+    "url": "/becker/index.html"
+  },
+  {
+    "revision": "d03b655b4ab40b8113f2",
+    "url": "/becker/static/css/main.fa74275b.chunk.css"
+  },
+  {
+    "revision": "9181c9e342c6b6177c71",
+    "url": "/becker/static/js/2.796f6177.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/becker/static/js/2.796f6177.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d03b655b4ab40b8113f2",
+    "url": "/becker/static/js/main.7eba8677.chunk.js"
+  },
+  {
+    "revision": "08fa701fdf9d1737022b",
+    "url": "/becker/static/js/runtime-main.c9236338.js"
+  },
+  {
+    "revision": "6dc4bf3aaca7b3d46b50a4ff2006f9a9",
+    "url": "/becker/static/media/as1.6dc4bf3a.jpg"
+  },
+  {
+    "revision": "0413e7370df03e7f5d024c72b87b64d8",
+    "url": "/becker/static/media/as2.0413e737.jpg"
+  },
+  {
+    "revision": "1066f45f87e9db42191b960272bb306c",
+    "url": "/becker/static/media/as3.1066f45f.jpg"
+  },
+  {
+    "revision": "f5a148b56db182c2be9a32295a075cf0",
+    "url": "/becker/static/media/bookBig.f5a148b5.jpg"
+  },
+  {
+    "revision": "de346fb0d275e801ba24b21f483fc21d",
+    "url": "/becker/static/media/bookSmall.de346fb0.jpg"
+  },
+  {
+    "revision": "9aa2be4eb40e449605a1d2b15fc23305",
+    "url": "/becker/static/media/bp1.9aa2be4e.jpg"
+  },
+  {
+    "revision": "dee4bf6affc081c98c0095a7a441c5b1",
+    "url": "/becker/static/media/bp2.dee4bf6a.jpg"
+  },
+  {
+    "revision": "666005b5ec2303ba1803464066b52f13",
+    "url": "/becker/static/media/bp3.666005b5.jpg"
+  },
+  {
+    "revision": "35b5be94f517c6bf68f959aeaf6eadd2",
+    "url": "/becker/static/media/cz1.35b5be94.jpg"
+  },
+  {
+    "revision": "2725d50341ea3f2a1805a1169c7d19ab",
+    "url": "/becker/static/media/cz2.2725d503.jpg"
+  },
+  {
+    "revision": "909ae43dfee7fafb0bafcad1bd599445",
+    "url": "/becker/static/media/heroDesktop.909ae43d.jpg"
+  },
+  {
+    "revision": "d3edf94233665c65794fb2a588ca3398",
+    "url": "/becker/static/media/heroMobile.d3edf942.jpg"
+  },
+  {
+    "revision": "ab0e575ece9015df2981920421dfd180",
+    "url": "/becker/static/media/ir1.ab0e575e.jpg"
+  },
+  {
+    "revision": "37503555e96c9f8ed5b64d0a71a19417",
+    "url": "/becker/static/media/ir2.37503555.jpg"
+  },
+  {
+    "revision": "9fabfa42e3d7d503fe54d0e21c8f6570",
+    "url": "/becker/static/media/ir3.9fabfa42.jpg"
+  },
+  {
+    "revision": "370b51523968eadb1a0b8673cb938592",
+    "url": "/becker/static/media/logo.370b5152.png"
+  },
+  {
+    "revision": "806753d57f65a291e440fd8ce74f356e",
+    "url": "/becker/static/media/mailpostBig.806753d5.jpg"
+  },
+  {
+    "revision": "7e3385420bd1002af987a34012f7a0c8",
+    "url": "/becker/static/media/mailpostSmall.7e338542.jpg"
+  },
+  {
+    "revision": "4d5ebeba8095d8dffb03c1fdaed0b4d3",
+    "url": "/becker/static/media/notesBig.4d5ebeba.jpg"
+  },
+  {
+    "revision": "319d8b1fbb48fce38bb7304718094d1d",
+    "url": "/becker/static/media/notesSmall.319d8b1f.jpg"
+  },
+  {
+    "revision": "a92535b515bb56fc811635e7ad120114",
+    "url": "/becker/static/media/nz1.a92535b5.jpg"
+  },
+  {
+    "revision": "9645ecc8889a22b662af0ba94d73f9aa",
+    "url": "/becker/static/media/nz2.9645ecc8.jpg"
+  },
+  {
+    "revision": "d8a964b56a5f71c491bc290001893133",
+    "url": "/becker/static/media/nz3.d8a964b5.jpg"
+  },
+  {
+    "revision": "c58890df17c1515705b73dc22553b6d7",
+    "url": "/becker/static/media/pencilBig.c58890df.jpg"
+  },
+  {
+    "revision": "85157c32db41bef137ed1dce7174f97f",
+    "url": "/becker/static/media/pencilSmall.85157c32.jpg"
+  },
+  {
+    "revision": "f4f0ccef0786c23ff46b1f481988630d",
+    "url": "/becker/static/media/wm1.f4f0ccef.jpg"
+  },
+  {
+    "revision": "4c1c0ac54112defb69ea450c27352712",
+    "url": "/becker/static/media/wm2.4c1c0ac5.jpg"
+  },
+  {
+    "revision": "b432630f9ce02c066d388a5a9d8870f6",
+    "url": "/becker/static/media/wm3.b432630f.jpg"
+  }
+]);
